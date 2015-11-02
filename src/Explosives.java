dcfc5d4e098764cb4b7d778e3a5f0f8a358e929d
smile <ismael.zakari.toure@gmail.com>
2015-11-03 00:01:59 +0100

    last commit

--- src/Explosives.java
+++ src/Explosives.java
@@ -67,14 +67,14 @@ public class Explosives{
     //@ requires prod1.startsWith("Prod") && prod2.startsWith("Prod");
 	//@ ensures ( (\result == true) ==> (\forall int d; 0 <= d && d < nb_inc; !(incomp[d][0].equals(prod1) && incomp[d][1].equals(prod2)) ) );
 	//@ ensures ( (\result == false) ==> (\exists int d; 0 <= d && d < nb_inc; incomp[d][0].equals(prod1) && incomp[d][1].equals(prod2) ) );
-	public boolean compatible(String prod1, String prod2){
+	public /*@ pure @*/ boolean compatible(String prod1, String prod2){
     	for(int i = 0; i < nb_inc; i++)
     		if(incomp[i][0].equals(prod1) && incomp[i][1].equals(prod2)) return false;
     	return true;
     }
     
     //@ ensures (\forall int f; 0<= f && f< nb_assign; !(\result.equals(assign[f][0])));
-    public String  newBat(){
+    public /*@ pure @*/ String  newBat(){
     	int cmpt = 0;
     	boolean alreadyExists = true;
     	while(alreadyExists){
@@ -90,7 +90,7 @@ public class Explosives{
     //@ ensures \result.startsWith("Bat");
     //@ ensures (nb_assign > 0) ==> ( (\result.equals(newBat())) <==> !(\exists int d; 0<= d && d< nb_assign; (\forall int f; 0<= f && f< nb_assign; (assign[d][0].equals(assign[f][0])) ==> compatible(assign[f][1],Prod)) ) ) || ( !(\result.equals(newBat())) <==> (\forall int f; 0<= f && f< nb_assign; (\forall int d; 0<= d && d< nb_assign; (assign[f][0].equals(assign[d][0])) ==> (compatible(assign[d][1],Prod)) ) ) );
     //@ ensures (nb_assign == 0) ==> ( \result.equals(newBat()) );
-    public String findBat(String Prod){
+    public /*@ pure @*/ String findBat(String Prod){
     	for(int i = 0; i < nb_assign; i++){
     		boolean compatible = true;
     		for(int j = 0; j < nb_assign; j++){
